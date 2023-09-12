a7847f54919d1cdbddc802c4e3ea5864339a061b
evpeople <hangcaihui@gmail.com>
2023-09-12 22:45:12 +0800

    实现RSS的修改

--- src/routes.rs
+++ src/routes.rs
@@ -1,24 +1,17 @@
-use crate::types::{CreateRss, CreateUser, RSSResult, User};
-use axum::{extract::Json, extract::State, http::StatusCode, response::IntoResponse};
+use crate::types::{CreateRss, CreateUser, User};
+use axum::{
+    body::Body,
+    extract::Json,
+    extract::State,
+    http::{Response, StatusCode},
+    response::IntoResponse,
+};
 use chrono::{DateTime, Local};
 use sqlx::SqlitePool;
 
 // basic handler that responds with a static string
 pub async fn root() -> &'static str {
     "Hello, World!"
-}
-
-pub async fn modify_rss(Json(payload): Json<CreateRss>) -> impl IntoResponse {
-    // insert your application logic here
-    let rss_result = RSSResult {
-        rss_name: "new RSS".to_string(),
-        id: 0,
-        url: payload.rss,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(rss_result))
 }
 pub async fn add_rss(
     State(pool): State<SqlitePool>,
@@ -154,4 +147,81 @@ pub async fn create_user(
             }),
         ),
     }
+}
+pub async fn modify_rss(rss_url: String) -> impl IntoResponse {
+    let rss_content = reqwest::get(&rss_url).await;
+    let rss_content = match rss_content {
+        Ok(rss_content) => match rss_content.text().await {
+            Ok(rss_content) => rss_content,
+            Err(e) => {
+                return Response::builder()
+                    .status(StatusCode::OK)
+                    .body(Body::from(
+                        serde_json::to_string(&User {
+                            error_msg: format!(
+                                "读取rss响应 {} 失败, error_msg是 {}",
+                                rss_url,
+                                e.to_string()
+                            ),
+                            username: "".to_string(),
+                        })
+                        .unwrap_or("json序列化失败".to_string()),
+                    ))
+                    .unwrap();
+            }
+        },
+        Err(e) => {
+            return Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::from(
+                    serde_json::to_string(&User {
+                        error_msg: format!(
+                            "抓取rss {} 失败, error_msg是 {}",
+                            rss_url,
+                            e.to_string()
+                        ),
+                        username: "".to_string(),
+                    })
+                    .unwrap_or("json序列化失败".to_string()),
+                ))
+                .unwrap();
+        }
+    };
+
+    let channel = rss::Channel::read_from(rss_content.as_bytes());
+    let channel = match channel {
+        Ok(channel) => channel,
+        Err(e) => {
+            return Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::from(
+                    serde_json::to_string(&User {
+                        error_msg: format!(
+                            "解析 rss {} 失败, error_msg是 {}",
+                            rss_url,
+                            e.to_string()
+                        ),
+                        username: "".to_string(),
+                    })
+                    .unwrap(),
+                ))
+                .unwrap();
+        }
+    };
+    // 修改 RSS 数据
+    let modified_items: Vec<rss::Item> = channel
+        .items()
+        .iter()
+        .map(|item| {
+            let mut new_item = item.clone();
+            new_item.set_title(format!("Modified: {}", item.title().unwrap_or_default()));
+            new_item
+        })
+        .collect();
+    let mut modified_channel = channel.clone();
+    modified_channel.set_items(modified_items);
+    Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from(modified_channel.to_string()))
+        .unwrap()
 }
